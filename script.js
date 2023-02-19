60cad8f57b449c9a5ff2c0a209127acef98c093e
Yuliia00 <skaskevich2000@gmail.com>
2023-02-19 13:50:13 +0100

    hw_4_changed

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 let number1 = prompt("Input number 1");
 let number2 = prompt("Input number 2");
-num1 = Number(number1)
-num2 = Number(number2)
+const num1 = Number(number1);
+const num2 = Number(number2);
 alert(`${num1}+${num2}=${num1+num2}
 ${num1}-${num2}=${num1-num2}
 ${num1}*${num2}=${num1*num2}
